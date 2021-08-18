ea30737cc6471e4c1510624ed27fa36a6b114d38
matsui54 <haru.matu9168@gmail.com>
2021-08-18 10:31:40 +0900

    Fix #1

--- denops/ddc/sources/buffer.ts
+++ denops/ddc/sources/buffer.ts
@@ -5,9 +5,21 @@ import {
   DdcOptions,
   SourceOptions,
 } from "https://deno.land/x/ddc_vim@v0.1.0/types.ts";
-import { Denops, fn } from "https://deno.land/x/ddc_vim@v0.1.0/deps.ts";
+import { Denops, fn, gather } from "https://deno.land/x/ddc_vim@v0.1.0/deps.ts";
+import { imap, range } from "https://deno.land/x/itertools@v0.1.3/mod.ts";
 
-function allWords(lines: string[]): string[] {
+export function splitPages(
+  minLines: number,
+  maxLines: number,
+  size: number,
+): Iterable<[number, number]> {
+  return imap(
+    range(minLines, /* < */ maxLines + 1, size),
+    (lnum: number) => [lnum, /* <= */ lnum + size - 1],
+  );
+}
+
+export function allWords(lines: string[]): string[] {
   return lines.flatMap((line) => [...line.matchAll(/[a-zA-Z0-9_]+/g)])
     .map((match) => match[0]).filter((e, i, self) => self.indexOf(e) === i);
 }
@@ -26,18 +38,28 @@ type bufCache = {
 
 export class Source extends BaseSource {
   private buffers: bufCache[] = [];
+  private pageSize = 500;
   events = ["BufReadPost", "BufWritePost", "InsertLeave"];
+
+  private async gatherWords(denops: Denops, endLine: number): Promise<Candidate[]> {
+    const ps = await gather(denops, async (denops) => {
+      for (const [s, e] of splitPages(1, endLine, this.pageSize)) {
+        await fn.getline(denops, s, e);
+      }
+    }) as string[][];
+    return allWords(ps.flatMap((p) => p)).map((word) => ({ word }));
+  }
 
   private async makeCache(
     denops: Denops,
     filetype: string,
     limit: number,
   ): Promise<void> {
-    const endLine = await fn.line(denops, "$") as number;
+    const endLine = await fn.line(denops, "$");
     const size = (await fn.line2byte(
       denops,
       endLine + 1,
-    ) as number) - 1;
+    )) - 1;
     if (size > limit) {
       return;
     }
@@ -46,9 +68,7 @@ export class Source extends BaseSource {
     this.buffers[bufnr] = {
       bufnr: bufnr,
       filetype: filetype,
-      candidates: allWords(
-        await fn.getline(denops, 1, endLine),
-      ).map((word) => ({ word })),
+      candidates: await this.gatherWords(denops, endLine),
     };
   }
 
@@ -92,6 +112,7 @@ export class Source extends BaseSource {
       tabBufnrs.includes(buf.bufnr) ||
       (params.fromAltBuf && (altbuf == buf.bufnr))
     );
+
     return buffers.map((buf) => buf.candidates).flatMap((candidate) =>
       candidate
     );
