f20da29bbd22f6d744a4bfa28227e2792045bcd4
matsui54 <haru.matu9168@gmail.com>
2021-08-27 23:32:12 +0900

    version up

--- denops/ddc/sources/deps.ts
+++ denops/ddc/sources/deps.ts
@@ -1,3 +1,3 @@
-export * from "https://deno.land/x/ddc_vim@v0.2.2/types.ts#^";
-export * from "https://deno.land/x/ddc_vim@v0.2.2/deps.ts#^";
+export * from "https://deno.land/x/ddc_vim@v0.3.0/types.ts#^";
+export * from "https://deno.land/x/ddc_vim@v0.3.0/deps.ts#^";
 export * from "https://deno.land/x/itertools@v0.1.3/mod.ts#^";
