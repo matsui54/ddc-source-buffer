19bc90d925a6b93030b28e36102e7367a9f3ba5f
matsui54 <haru.matu9168@gmail.com>
2021-09-09 22:35:55 +0900

    Add test

--- denops/ddc-sources/tests/buffer_test.ts
+++ denops/ddc-sources/tests/buffer_test.ts
@@ -1,13 +1,12 @@
 import {
-  assertEquals
+  assert,
+  assertEquals,
 } from "https://deno.land/std@0.106.0/testing/asserts.ts#^";
-import { allWords, splitPages } from "../buffer.ts";
+import { allWords, getFileSize } from "../buffer.ts";
 
-Deno.test("pages", () => {
-  assertEquals(Array.from(splitPages(1, 600, 500)), [[1, 500], [501, 1000]]);
-  assertEquals(Array.from(splitPages(1, 1, 500)), [[1, 500]]);
-  assertEquals(Array.from(splitPages(1, 500, 500)), [[1, 500]]);
-  assertEquals(Array.from(splitPages(1, 501, 500)), [[1, 500], [501, 1000]]);
+Deno.test("getFileSize", async () => {
+  assert(await getFileSize("./README.md") > 0);
+  assert(await getFileSize("./non-exists") == -1);
 });
 
 Deno.test("allWords", () => {
