695cf9e14a41e19a78a12adb78ed35788307b7ac
matsui54 <matsui54@users.noreply.github.com>
2022-03-26 00:29:33 +0000

    :package: Update Deno dependencies
    Update dependencies by udd:
    
    /home/runner/work/ddc-buffer/ddc-buffer/denops/@ddc-sources/tests/buffer_test.ts
    [1/1] Looking for releases: https://deno.land/std@0.131.0/testing/asserts.ts
    [1/1] Attempting update: https://deno.land/std@0.131.0/testing/asserts.ts -> 0.132.0
    [1/1] Update successful: https://deno.land/std@0.131.0/testing/asserts.ts -> 0.132.0
    
    /home/runner/work/ddc-buffer/ddc-buffer/denops/@ddc-sources/buffer.ts
    [1/5] Looking for releases: https://deno.land/x/ddc_vim@v2.2.0/types.ts
    [1/5] Using latest: https://deno.land/x/ddc_vim@v2.2.0/types.ts
    [2/5] Looking for releases: https://deno.land/x/ddc_vim@v2.2.0/base/source.ts
    [2/5] Using latest: https://deno.land/x/ddc_vim@v2.2.0/base/source.ts
    [3/5] Looking for releases: https://deno.land/x/denops_std@v3.2.0/function/mod.ts
    [3/5] Using latest: https://deno.land/x/denops_std@v3.2.0/function/mod.ts
    [4/5] Looking for releases: https://deno.land/x/denops_std@v3.2.0/mod.ts
    [4/5] Using latest: https://deno.land/x/denops_std@v3.2.0/mod.ts
    [5/5] Looking for releases: https://deno.land/std@0.131.0/path/mod.ts
    [5/5] Attempting update: https://deno.land/std@0.131.0/path/mod.ts -> 0.132.0
    [5/5] Update successful: https://deno.land/std@0.131.0/path/mod.ts -> 0.132.0
    
    /home/runner/work/ddc-buffer/ddc-buffer/README.md
    
    Already latest version:
    https://deno.land/x/ddc_vim@v2.2.0/types.ts == v2.2.0
    https://deno.land/x/ddc_vim@v2.2.0/base/source.ts == v2.2.0
    https://deno.land/x/denops_std@v3.2.0/function/mod.ts == v3.2.0
    https://deno.land/x/denops_std@v3.2.0/mod.ts == v3.2.0
    
    Successfully updated:
    https://deno.land/std@0.131.0/testing/asserts.ts 0.131.0 -> 0.132.0
    https://deno.land/std@0.131.0/path/mod.ts 0.131.0 -> 0.132.0
    make[1]: Entering directory '/home/runner/work/ddc-buffer/ddc-buffer'
    make[1]: Leaving directory '/home/runner/work/ddc-buffer/ddc-buffer'

--- denops/@ddc-sources/tests/buffer_test.ts
+++ denops/@ddc-sources/tests/buffer_test.ts
@@ -1,7 +1,7 @@
 import {
   assert,
   assertEquals,
-} from "https://deno.land/std@0.131.0/testing/asserts.ts";
+} from "https://deno.land/std@0.132.0/testing/asserts.ts";
 import { allWords, getFileSize } from "../buffer.ts";
 
 Deno.test("getFileSize", async () => {
