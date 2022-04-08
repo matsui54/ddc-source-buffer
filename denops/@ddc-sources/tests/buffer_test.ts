e0708fd043c82e7007e07060d44ca81fbb61b6d7
matsui54 <matsui54@users.noreply.github.com>
2022-04-08 00:34:03 +0000

    :package: Update Deno dependencies
    Update dependencies by udd:
    
    /home/runner/work/ddc-buffer/ddc-buffer/denops/@ddc-sources/buffer.ts
    [1/4] Looking for releases: https://deno.land/x/ddc_vim@v2.2.0/types.ts
    [1/4] Using latest: https://deno.land/x/ddc_vim@v2.2.0/types.ts
    [2/4] Looking for releases: https://deno.land/x/ddc_vim@v2.2.0/deps.ts
    [2/4] Using latest: https://deno.land/x/ddc_vim@v2.2.0/deps.ts
    [3/4] Looking for releases: https://deno.land/x/ddc_vim@v2.2.0/base/source.ts
    [3/4] Using latest: https://deno.land/x/ddc_vim@v2.2.0/base/source.ts
    [4/4] Looking for releases: https://deno.land/std@0.133.0/path/mod.ts
    [4/4] Attempting update: https://deno.land/std@0.133.0/path/mod.ts -> 0.134.0
    [4/4] Update successful: https://deno.land/std@0.133.0/path/mod.ts -> 0.134.0
    
    /home/runner/work/ddc-buffer/ddc-buffer/denops/@ddc-sources/tests/buffer_test.ts
    [1/1] Looking for releases: https://deno.land/std@0.133.0/testing/asserts.ts
    [1/1] Attempting update: https://deno.land/std@0.133.0/testing/asserts.ts -> 0.134.0
    [1/1] Update successful: https://deno.land/std@0.133.0/testing/asserts.ts -> 0.134.0
    
    /home/runner/work/ddc-buffer/ddc-buffer/README.md
    
    Already latest version:
    https://deno.land/x/ddc_vim@v2.2.0/types.ts == v2.2.0
    https://deno.land/x/ddc_vim@v2.2.0/deps.ts == v2.2.0
    https://deno.land/x/ddc_vim@v2.2.0/base/source.ts == v2.2.0
    
    Successfully updated:
    https://deno.land/std@0.133.0/path/mod.ts 0.133.0 -> 0.134.0
    https://deno.land/std@0.133.0/testing/asserts.ts 0.133.0 -> 0.134.0
    make[1]: Entering directory '/home/runner/work/ddc-buffer/ddc-buffer'
    make[1]: Leaving directory '/home/runner/work/ddc-buffer/ddc-buffer'

--- denops/@ddc-sources/tests/buffer_test.ts
+++ denops/@ddc-sources/tests/buffer_test.ts
@@ -1,7 +1,7 @@
 import {
   assert,
   assertEquals,
-} from "https://deno.land/std@0.133.0/testing/asserts.ts";
+} from "https://deno.land/std@0.134.0/testing/asserts.ts";
 import { allWords, getFileSize } from "../buffer.ts";
 
 Deno.test("getFileSize", async () => {
