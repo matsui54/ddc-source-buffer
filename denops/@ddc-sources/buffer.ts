9df18ccc37ed8dca6c1523b02ee510df480b9d3a
matsui54 <matsui54@users.noreply.github.com>
2022-09-11 00:43:28 +0000

    :package: Update Deno dependencies
    Update dependencies by udd:
    
    /home/runner/work/ddc-buffer/ddc-buffer/denops/@ddc-sources/buffer.ts
    [1/4] Looking for releases: https://deno.land/x/ddc_vim@v2.3.0/types.ts
    [1/4] Attempting update: https://deno.land/x/ddc_vim@v2.3.0/types.ts -> v2.5.1
    [1/4] Update successful: https://deno.land/x/ddc_vim@v2.3.0/types.ts -> v2.5.1
    [2/4] Looking for releases: https://deno.land/x/ddc_vim@v2.3.0/deps.ts
    [2/4] Attempting update: https://deno.land/x/ddc_vim@v2.3.0/deps.ts -> v2.5.1
    [2/4] Update successful: https://deno.land/x/ddc_vim@v2.3.0/deps.ts -> v2.5.1
    [3/4] Looking for releases: https://deno.land/x/ddc_vim@v2.3.0/base/source.ts
    [3/4] Attempting update: https://deno.land/x/ddc_vim@v2.3.0/base/source.ts -> v2.5.1
    [3/4] Update successful: https://deno.land/x/ddc_vim@v2.3.0/base/source.ts -> v2.5.1
    [4/4] Looking for releases: https://deno.land/std@0.146.0/path/mod.ts
    [4/4] Attempting update: https://deno.land/std@0.146.0/path/mod.ts -> 0.155.0
    [4/4] Update successful: https://deno.land/std@0.146.0/path/mod.ts -> 0.155.0
    
    /home/runner/work/ddc-buffer/ddc-buffer/denops/@ddc-sources/tests/buffer_test.ts
    [1/1] Looking for releases: https://deno.land/std@0.146.0/testing/asserts.ts
    [1/1] Attempting update: https://deno.land/std@0.146.0/testing/asserts.ts -> 0.155.0
    [1/1] Update successful: https://deno.land/std@0.146.0/testing/asserts.ts -> 0.155.0
    
    /home/runner/work/ddc-buffer/ddc-buffer/README.md
    
    Successfully updated:
    https://deno.land/x/ddc_vim@v2.3.0/types.ts v2.3.0 -> v2.5.1
    https://deno.land/x/ddc_vim@v2.3.0/deps.ts v2.3.0 -> v2.5.1
    https://deno.land/x/ddc_vim@v2.3.0/base/source.ts v2.3.0 -> v2.5.1
    https://deno.land/std@0.146.0/path/mod.ts 0.146.0 -> 0.155.0
    https://deno.land/std@0.146.0/testing/asserts.ts 0.146.0 -> 0.155.0
    make[1]: Entering directory '/home/runner/work/ddc-buffer/ddc-buffer'
    make[1]: Leaving directory '/home/runner/work/ddc-buffer/ddc-buffer'

--- denops/@ddc-sources/buffer.ts
+++ denops/@ddc-sources/buffer.ts
@@ -2,13 +2,13 @@ import {
   BaseSource,
   DdcEvent,
   Item,
-} from "https://deno.land/x/ddc_vim@v2.3.0/types.ts";
-import { Denops, fn } from "https://deno.land/x/ddc_vim@v2.3.0/deps.ts";
+} from "https://deno.land/x/ddc_vim@v2.5.1/types.ts";
+import { Denops, fn } from "https://deno.land/x/ddc_vim@v2.5.1/deps.ts";
 import {
   GatherArguments,
   OnEventArguments,
-} from "https://deno.land/x/ddc_vim@v2.3.0/base/source.ts";
-import { basename } from "https://deno.land/std@0.146.0/path/mod.ts";
+} from "https://deno.land/x/ddc_vim@v2.5.1/base/source.ts";
+import { basename } from "https://deno.land/std@0.155.0/path/mod.ts";
 
 export async function getFileSize(fname: string): Promise<number> {
   let file: Deno.FileInfo;
