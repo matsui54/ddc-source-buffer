9a1614d4cbf88f7209a082b2330d9dc44f081c76
Shougo Matsushita <Shougo.Matsu@gmail.com>
2023-06-27 09:06:53 +0900

    Fix for the latest ddc

--- denops/@ddc-sources/tests/buffer_test.ts
+++ denops/@ddc-sources/tests/buffer_test.ts
@@ -1,7 +1,7 @@
 import {
   assert,
   assertEquals,
-} from "https://deno.land/std@0.187.0/testing/asserts.ts";
+} from "https://deno.land/std@0.192.0/testing/asserts.ts";
 import { allWords, getFileSize } from "../buffer.ts";
 
 Deno.test("getFileSize", async () => {
